3feb492b8b19482d186b2fe56a98e8f0dd1a2474
ruslan123 <perishemizade@gmail.com>
2023-03-27 11:26:14 +0400

    number yuklendi

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// 1. is_int(tam  sayi)
+// $x = 7987; (79.87 ise tam sayi deyil)
+// if(is_int($x)){
+//    echo $x."sayisi tam sayidir";
+// } else{
+//    echo $x."sayisi tam sayi deyil";
+// }
+// 2. is_float(ondaliqli ve ya ustel sayilardir) is_double takma adidir
+// $x = 79.87; (7956 ise ondaliqli deyil)
+// if(is_float($x)){
+//    echo $x."sayisi ondaliqli sayidir";
+// } else{
+//    echo $x."sayisi ondaliqli deyil";
+// }
+// 3. is_infinite(sonsuz sayi olub olmadigini gosterir)
+// 4. is_nan(sayi olub olmadigini gosterir)
+//  $x= 8;
+//  if(is_nan($x) == 0){
+//    echo $x."sayisi sayidir";
+// } else{
+//    echo $x."sayisi sayi deyil";
+// }
+// 5. is_numeric(sayisal ifade olub olmadigini gosterir)
+// $x=0xf4c300c;
+//  if(is_numeric($x)){
+//    echo $x."sayisi sayisal sayidir";
+// } else{
+//    echo $x."sayisi sayisal sayi deyil";
+// }
+// 6. int tam sayiya cevirir;
+// $x=78.85;
+// echo (int)$x
+// echo intval(7.8)
+// math Funksiyonlara ise google-da math function php yazanda hamisi gelir
+?>
